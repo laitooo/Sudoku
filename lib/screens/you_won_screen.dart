049d7c307b887f8ab4cf33e6badf7ffe9e308798
laitooo <killuasan25@gmail.com>
2021-11-25 10:00:14 +0200

    handled back button in result screen

--- lib/screens/you_won_screen.dart
+++ lib/screens/you_won_screen.dart
@@ -18,46 +18,56 @@ class YouWonScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'You won in $minutes minutes and $seconds seconds',
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) {
+            return HomeScreen();
+          }),
+        );
+        return false;
+      },
+      child: Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'You won in $minutes minutes and $seconds seconds',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 150),
-            MainButton(
-              text: 'Play again',
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) {
-                    return PlayScreen(
-                      mode: mode,
-                    );
-                  }),
-                );
-              },
-            ),
-            SizedBox(height: 30),
-            MainButton(
-              text: 'Go home',
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) {
-                    return HomeScreen();
-                  }),
-                );
-              },
-            ),
-          ],
+              SizedBox(height: 150),
+              MainButton(
+                text: 'Play again',
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) {
+                      return PlayScreen(
+                        mode: mode,
+                      );
+                    }),
+                  );
+                },
+              ),
+              SizedBox(height: 30),
+              MainButton(
+                text: 'Go home',
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) {
+                      return HomeScreen();
+                    }),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
